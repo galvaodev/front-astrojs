316be34012112a3b800502fc2f1f14e3116b7657
João Victor <galvaopp@gmail.com>
2024-06-18 22:46:32 -0300

    fix: layout (#1)

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,17 +9,32 @@ export default function Card() {
   const character = useStore(characterName);
 
   return (
-    <>
-      <div className="flex flex-wrap bg-slate-800 rounded-xl p-8">
-        <Suspense fallback={<Fallback />}>
-          <p className="text-lg font-medium text-slate-50 columns-12">
-           {character.character}
-          </p>
-          <p className="text-lg font-medium text-slate-50  columns-12">
-           {character.world}
-          </p>
-        </Suspense>
-      </div>
-    </>
+    <div className="flex flex-wrap bg-slate-800 rounded-xl h-full p-8">
+      <Suspense fallback={<Fallback />}>
+        {character.character ? (
+          <div>
+            <p class="text-slate-50 font-medium">Nome:</p>
+            <p className="text-3xl font-body font-medium text-slate-50 columns-12">
+              {character.character}
+            </p>
+            <p class="text-slate-50 font-medium mt-5">Mundo:</p>
+            <p className="text-3xl font-body font-medium text-slate-50  columns-12">
+              {character.world}
+            </p>
+            <p class="text-slate-50 font-medium mt-5">Level:</p>
+            <p className="text-3xl font-body font-medium text-slate-50  columns-12">
+              {character.level}
+            </p>
+
+            <p class="text-slate-50 font-medium mt-5">Vocação:</p>
+            <p className="text-3xl font-body font-medium text-slate-50  columns-12">
+              {character.vocation}
+            </p>
+          </div>
+        ) : (
+          <p class="text-slate-50 font-medium text-center w-full">Personagem não encontrado</p>
+        )}
+      </Suspense>
+    </div>
   );
 }
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,19 +3,31 @@ import { characterName } from '../../context/storeCharacter';
 
 export default function Form() {
     const character = characterName;
-    const handleBlur = async (event: any) => {
-        const inputValue = event.target.value;
+    const onSubmit = async (event: any) => {
+        event.preventDefault();
+        const inputValue = event.target.character.value;
         const teste = await getApi(inputValue) as any
         const response = await teste.json();
 
         if (response.character.character.name) {
-            character.set({character: response.character.character.name, world: response.character.character.world});
+            character.set({
+                character: response.character.character.name,
+                world: response.character.character.world,
+                vocation: response.character.character.vocation,
+                level: response.character.character.level
+            });
         }
     };
 
     return (
-        <form>
-            <input type="text" name="inputName" onBlur={(e) => handleBlur(e)} required />
+        <form class="w-full block py-5" onSubmit={onSubmit}>
+            <input
+                placeholder="Deseja procurar qual personagem?"
+                class="border-gray-800 border-2 p-5 w-10/12"
+                type="text"
+                name="character" required />
+
+            <button class="w-2/12 p-5 bg-gray-800 border-2 border-gray-800 text-white" type="submit">Procurar</button>
         </form>
     )
 }
