0d335b3e20bc20c436cf9b0c34e56bb72b7b6477
modesto <galvaopp@gmail.com>
2024-06-18 22:44:29 -0300

    fix: layout

--- src/context/storeCharacter.tsx
+++ src/context/storeCharacter.tsx
@@ -1,7 +1,12 @@
 import { atom } from "nanostores";
 
-const initialValue = { character: '', world: ''};
+interface IInitial {
+    character?: string;
+    world?: string;
+    vocation?: string;
+    level?: string;
+}
 
+const initialValue: IInitial = { character: '', world: '', vocation: '', level: '' };
 const characterName = atom(initialValue);
-
 export { characterName };
